5e4b56eb37e0ddb10ca365bb7cdd541302836a15
Jonnas <jonnaschristian@gmail.com>
2023-11-11 18:02:22 -0300

    conexao com o banco

--- app/app.py
+++ app/app.py
@@ -1,16 +1,37 @@
-from flask import jsonify
-from flask_migrate import Migrate
+from flask import Flask, Response, request
+from flask_sqlalchemy import SQLAlchemy
+import json
 
-from app import app, db
-from app.models import User
+app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://dev:postgres@localhost:5432/uecebet'
+app.app_context()
+db = SQLAlchemy(app)
 
-Migrate(app, db)
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key= True)
+    nome = db.Column(db.String(50))
+    email = db.Column(db.String(100))
 
-@app.make_shell_context
-def make_shell_context():
-    return dict(
-        app=app,
-        db=db,
-        User=User
-    )
+    def to_json(self):
+        return {"id": self.id, "nome": self.nome, "email": self.email}
 
+# Selecionar Tudo
+@app.route("/usuarios", methods=["GET"])
+def seleciona_usuarios():
+    usuarios_objetos = Usuario.query.all()
+    usuarios_json = [usuario.to_json() for usuario in usuarios_objetos]
+    print(usuarios_json)
+    return gera_response(200, "Usuarios", usuarios_json, "ok")
+
+
+def gera_response(status, nome_do_conteudo, conteudo, mensagem=False):
+    body = {}
+    body[nome_do_conteudo] = conteudo
+
+    if(mensagem):
+        body["mensagem"] = mensagem
+
+    return Response(json.dumps(body), status=status, mimetype="application/json")
+
+app.run()
