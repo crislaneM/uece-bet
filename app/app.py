7e7fbee6466223a2fa26626ba7a4679cb9049acc
Jonnas <jonnaschristian@gmail.com>
2023-11-12 10:04:42 -0300

    database ok

--- app/app.py
+++ app/app.py
@@ -12,9 +12,16 @@ class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key= True)
     nome = db.Column(db.String(50))
     email = db.Column(db.String(100))
+    teste = db.Column(db.String(100))
 
     def to_json(self):
         return {"id": self.id, "nome": self.nome, "email": self.email}
+
+class teste(db.Model):
+    id = db.Column(db.Integer, primary_key= True)
+    nome = db.Column(db.String(50))
+    email = db.Column(db.String(100))
+    teste = db.Column(db.String(100))
 
 # Selecionar Tudo
 @app.route("/usuarios", methods=["GET"])
