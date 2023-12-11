3bb4a36328ea519e22238736b9dd597787a677eb
Thales Lopes <thales.lopes@aluno.uece.br>
2023-12-10 22:05:52 -0300

    aaaaaa

--- app/controllers/wallet_controller.py
+++ app/controllers/wallet_controller.py
@@ -1,6 +1,7 @@
 from app.models.models_db import *
 from app.schemas.wallet_schemas import *
 from flask_restx import Resource, Namespace
+from app.controllers.helper import verificar_permissao_admin
 from flask_jwt_extended import get_jwt_identity, jwt_required
 from flask import request
 
@@ -38,10 +39,12 @@ class deposit(Resource):
         else:
             return {'message': 'Usuário não encontrado'}, 404
         
-@carteira_ns.route("/depositar/adm/<int:id>")
+@carteira_ns.route("/depositar/adm")
 class deposit(Resource):
     @carteira_ns.expect(deposit_status)
-    def post(self, id):
+    @jwt_required()
+    def post(self):
+        verificar_permissao_admin()
         caixa = Caixa.query.filter_by(id=1).first()
         deposit_data = carteira_ns.payload
         valor_deposito = deposit_data['deposito']
